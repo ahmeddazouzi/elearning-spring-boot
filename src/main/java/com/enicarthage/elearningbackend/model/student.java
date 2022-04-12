82a0fdb3202bed3a06ad5298eeff94dd2bb1274d
ahmedazouzi <ahmedazouzi97@gmail.com>
2022-04-12 19:54:54 +0100

    make a student class as a entity in the database

--- src/main/java/com/enicarthage/elearningbackend/model/student.java
+++ src/main/java/com/enicarthage/elearningbackend/model/student.java
@@ -1,5 +1,10 @@
 package com.enicarthage.elearningbackend.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "student")
 public class student {
 
     private long id ;
